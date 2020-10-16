6f78337ee64eea727fb032aaf183be6ab6ee5fb7
liuxiaoming <654554138@qq.com>
2020-10-16 17:47:59 +0800

    spring boot helloword

---
+++ src/main/java/com/cebbank/liuxiaoming/springboot/HelloWorldMainApplication.java
@@ -0,0 +1,14 @@
+package com.cebbank.liuxiaoming.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @SpringBootApplication 标注主程序类
+ */
+@SpringBootApplication
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldMainApplication.class);
+    }
+}
