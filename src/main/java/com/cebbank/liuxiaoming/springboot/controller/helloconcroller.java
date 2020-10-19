3151ac4bb20df89f74ab27c63364c2213f5ba28f
liuxiaoming <654554138@qq.com>
2020-10-19 17:36:41 +0800

    补充静态资源，

--- src/main/java/com/cebbank/liuxiaoming/springboot/controller/helloconcroller.java
+++ src/main/java/com/cebbank/liuxiaoming/springboot/controller/helloconcroller.java
@@ -1,13 +1,30 @@
 package com.cebbank.liuxiaoming.springboot.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Arrays;
+import java.util.Map;
+
+@Controller
 public class helloconcroller {
 
     @RequestMapping("/hello")
     public  String hello(){
         return "hello world quick";
     }
+
+
+    @RequestMapping("/success")
+    public String success(Map<String,Object> map){
+        map.put("hello","你好");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
+        return "success";
+    }
+
+//    @RequestMapping(value = {"/","/index.html"})
+//    public String index(){
+//        return "index";
+//    }
+
 }
